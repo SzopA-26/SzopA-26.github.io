03c301bfb5788eb7cd67d8b7191f7649cad7e3c5
Zee <patiphat.kh@ku.th>
2020-01-07 04:20:20 +0700

    update Lab03

--- Lab03/script.js
+++ Lab03/script.js
@@ -11,27 +11,95 @@ setInterval( function() {
 var hColor = 1;
 setInterval( function() {
     if (hColor > 0) {
-        document.getElementById("header").style.color = "#2f00ff";
+        document.getElementById("header").style.color = "#fff";
     } else {
-        document.getElementById("header").style.color = "#d13434";
+        document.getElementById("header").style.color = "#61a335";
     }
     hColor *= -1;
-}, 150 );
+}, 250 );
 
 function labelCheck() {
     let giftcheck = document.getElementById("gift-check");
     giftcheck.checked = !giftcheck.checked;
 }
 
+var Guests = [];
+var Gifts = [];
 function addBtnOnClicked() {
     let name = document.getElementById("name-input").value;
     let giftcheck = document.getElementById("gift-check");
-    document.getElementById("name-input").value = "";
-    if (giftcheck.checked) {
-        console.log("check")
-        giftcheck.checked = false;
+    
+    if (name == "") {
+        window.alert("Name is empty.")
     }
-    console.log(name)
+    else if (Guests.indexOf(name) != -1) {
+        window.alert("This name is already exist.")
+    } else {
+        Guests.push(name);
+        let nodeLi = document.createElement("li");
+        let text = document.createTextNode(name+" ");
+        nodeLi.appendChild(text);
+        
+        if (giftcheck.checked) {
+            Gifts.push(1);
+            let nodeI = document.createElement("i");
+            nodeI.className = "fas fa-gift";
+            nodeLi.appendChild(nodeI);
+        } else {
+            Gifts.push(0);
+        }
+        document.getElementById("name-list").appendChild(nodeLi);
+    }
+    
+    document.getElementById("name-input").value = "";
+    giftcheck.checked = false;
+    document.getElementById("list-header").innerHTML = "ผู้เข้าร่วมงาน(" + Guests.length + ")";
 }
 
-console.log("test")
+function ranBtnOnClicked() {
+    if (Guests.length < 2)
+        return;
+    let ranGuests = [];
+    let NotGive = [];
+    for (let i=0; i<Guests.length; i++) {
+        if (Gifts[i] == 1) {
+            ranGuests.push(Guests[i]);
+            NotGive.push(Guests[i]);
+        }
+    }
+
+    document.getElementById("random-list").innerHTML = 
+    '<button type="button" class="btn btn-primary" id="ranBtn" onclick="ranBtnOnClicked()">เริ่มสุ่มของขวัญ</button>';
+    
+    for (let i=0; i<ranGuests.length; i++) {
+        let guestGiven;
+        do {
+            guestGiven = NotGive[Math.floor(Math.random() * NotGive.length)];
+        } while (ranGuests[i] == guestGiven)
+        NotGive.splice(NotGive.indexOf(guestGiven) ,1);
+
+        let node = document.createElement("div");
+        let text = document.createTextNode(ranGuests[i]+" <--- ")
+        node.appendChild(text);
+        let nodeI = document.createElement("i");
+        nodeI.className = "fas fa-gift";
+        node.appendChild(nodeI);
+        text = document.createTextNode(" ---- "+guestGiven);
+        node.appendChild(text);
+        document.getElementById("random-list").appendChild(node);
+    }
+    
+    let node = document.createElement("HR");
+    document.getElementById("random-list").appendChild(node);
+    
+    for (let i=0; i<ranGuests.length; i++) {
+        console.log(ranGuests[i]);
+    }
+}
+
+var name_input = document.getElementById("name-input");
+name_input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        document.getElementById("addBtn").click();
+    }
+});
